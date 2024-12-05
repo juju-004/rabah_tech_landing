6c30be0404e6c0912c13ec3a5bdaa4a522bbd7d9
David Udoh <udohdavid004@gmail.com>
2024-12-05 12:20:42 +0100

    fix six

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./style.css";
+import "./bootstrap.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
